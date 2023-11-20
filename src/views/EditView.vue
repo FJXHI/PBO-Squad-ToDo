2b2ba1539cd13813538686e9246f4d93a36e29fe
FJXHI <s82049@informatik.htw-dresden.de>
2023-11-20 15:06:54 +0100

    add function for Inputs - close #2

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -1,44 +1,87 @@
-<script setup lang="ts">
-import ComButton from '../components/ComButton.vue'
+<script lang="ts">
+import editButton from '@/components/editButton.vue';
+export default {
+    components: {
+        editButton,
+    },
+    data() {
+        return {
+            inputName: '',
+            inputDate: '',
+            inputTime: '',
+            inputDescrript: '',
+            inputTags: ''
+        }
+    },
+    methods: {
+    saveEdit() {
+      if (this.inputName.trim() !== '') {
+        const output = {
+          name: this.inputName,
+          date: this.inputDate,
+          time: this.inputTime,
+          description: this.inputDescrript,
+          tags: this.inputTags,
+        }; 
+        console.log(output);
+        this.clearInput(); // Clear Inputs after Save
+      }
+    },
+    cancelEdit() {
+      this.clearInput(); // Clear Inputs after Cancel
+      this.$router.push('/'); //Return to Home
+    },
+    clearInput() {
+      this.inputName = '';
+      this.inputDate = '';
+      this.inputTime = '';
+      this.inputDescrript = '';
+      this.inputTags = '';
+    }
+  },
+};
+ 
+
+
 </script>
 
 <template>
   <div class="edit">
     <div class="edit-field">
-      <h1>Edit</h1>
-      
-      <form>
-        <div class="edit_btn">
-          <button class="btn_save">Speichern</button>
-          <button class="btn_cancel">Abbruch</button>
-          
-
-
-        </div>
-        <label for="f_name">Name</label>
-			  <input class="user-input" type="text" id="id_name" name="idn_name" placeholder="Name">
-        <label for="f_date">Datum</label>
-			  <input class="user-input" type="datetime-local" id="id_date" name="idn_date" placeholder="Datum">
-        <br>
-        <label for="f_time">Zeitaufwand</label>
-			  <input class="user-input" type="number" id="id_time" name="idn_time" placeholder="Zeitaufwand" min="0">
-			  <label for="f_text">Beschreibung</label>
-			  <input class="user-input" type="text" id="id_text" name="idn_text" placeholder="Beschreibung">
+      <div><!--<form> Err: Form submission canceled because the form is not connected -->
+        <span class="edit_btn">
+          <editButton class="btn_cancel" @click=cancelEdit>Cancel</editButton>          
+          <editButton class="btn_save" @click=saveEdit>Save</editButton>
+        </span>
+        <label for="id_name">Name</label>
+			  <input class="user-input" type="text" v-model="inputName" placeholder="Name" required>
+        <label for="f_date">Deadline Date & Time</label>
+			  <input class="user-input" type="datetime-local" v-model="inputDate" placeholder="Date">
+        <label for="f_time">Time expenditure</label>
+			  <input class="user-input" type="number" v-model="inputTime" placeholder="Time expenditure" min="0">
+			  <label for="f_text">Description</label>
+			  <input class="user-input" type="text" v-model="inputDescrript" placeholder="Description">
         <label for="f_tags">Tags</label>
-			  <input class="user-input" type="text" id="id_tags" name="idn_tags" placeholder="Tags">
-      </form>
+			  <input class="user-input" type="text" v-model="inputTags" placeholder="Tags">
+      </div>
     </div>
   </div>
 </template>
 
 <style>
+/*
 @media (min-width: 1024px) {
   .edit {
     min-height: 100vh;
     display: flex;
     align-items: center;
-    /*background-color: black;*/
   }
+}
+*/
+
+label {
+  color: #808080;
+  font-size: 18px;
 }
 
 .user-input{
@@ -50,20 +93,11 @@ import ComButton from '../components/ComButton.vue'
   border: none;
   background: #1C1C1E;
   color: #808080;
-  /* 
-  color: #fff;
-  background: rgba(255, 255, 255, 0.2);
-  text-align: center;
-  border: 1px solid #ccc;
-  
-  */
   font-size: 18px;
   margin: 8px 0;
   display: inline-block;
   border-radius: 5px;
-  box-sizing: border-box;
-
-  
+  box-sizing: border-box;  
   transition: 0.5s;
   transition-property: border-left, border-right, box-shadow;
 }
@@ -74,7 +108,10 @@ import ComButton from '../components/ComButton.vue'
   box-shadow: 0 0 100px rgba(66, 133, 244, 0.9);
 }
 
-
+.edit_btn {
+  display: flex;
+  justify-content: space-around;
+}
 
 .edit_btn button {
   outline: none;
