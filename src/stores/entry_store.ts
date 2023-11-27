26e71b4fc9da529fc6ef4599dc9a1eca0753be29
HansLuft778 <hendrik.lante@gmail.com>
2023-11-27 21:35:17 +0100

    added remove function + update gitignore

--- src/stores/entry_store.ts
+++ src/stores/entry_store.ts
@@ -18,19 +18,26 @@ export interface ToDoEntry {
     color: Color;
 
     deadline?: Date;
-    expenditure?: {time: number, unit: string}
+    expenditure?: { time: number, unit: string }
 
     flatPriority?: number;
-    
+
     tags?: Tag[];
 };
 
 export const useToDoEntryStore = defineStore('todoEntries', () => {
     const entries = ref<ToDoEntry[]>([]);
 
-    function addEntry(entry : ToDoEntry) : void {
+    function addEntry(entry: ToDoEntry): void {
         entries.value.push(entry);
     }
-  
+
+    function removeEntry(entry: ToDoEntry): void {
+        const index = entries.value.indexOf(entry);
+        if (index > -1) {
+            entries.value.splice(index, 1);
+        }
+    }
+
     return { entries, addEntry }
 })
