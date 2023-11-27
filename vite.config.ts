4b86c9c6e4dcea50d6cc14833b3d71b9dd9a5a53
HansLuft778 <hendrik.lante@gmail.com>
2023-11-27 17:49:57 +0100

    added minimal file requirements for Service Worker

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,29 @@ export default defineConfig({
   base: '/PBO-Squad-ToDo/',
   plugins: [
     vue(),
+    // VitePWA({
+    //   injectRegister: 'auto'
+    // }),
     VitePWA({
-      injectRegister: 'auto'
+      includeAssets: ['favicon.ico', 'logo-512x512.png', 'logo.svg'],
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#000',
+        icons: [
+          {
+            src: 'logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     }),
   ],
   resolve: {
