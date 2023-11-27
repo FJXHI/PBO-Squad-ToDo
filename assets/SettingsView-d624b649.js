547f88399708c02ba5365b6b526e907ae765a43c
4rgos <4rgos@users.noreply.github.com>
2023-11-27 12:22:08 +0000

    deploy: 32e2b2b05cfa40b526c1ac4611939f42e8e861fc

---
+++ assets/SettingsView-d624b649.js
@@ -0,0 +1,1 @@
+import{_ as s,o as t,c as _,p as o,g as c,b as a}from"./index-961ef1e0.js";const n={},i=e=>(o("data-v-4a2beb34"),e=e(),c(),e),d={class:"settings"},p=i(()=>a("h1",null,"This is an settings page",-1)),r=[p];function l(e,h){return t(),_("div",d,r)}const g=s(n,[["render",l],["__scopeId","data-v-4a2beb34"]]);export{g as default};
