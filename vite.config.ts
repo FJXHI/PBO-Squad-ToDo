deb02f0b3613076ddc606f246ec301d79585c991
HansLuft778 <hendrik.lante@gmail.com>
2023-11-27 08:34:33 +0100

    Added PWA module

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/PBO-Squad-ToDo/',
   plugins: [
     vue(),
+    VitePWA({
+      injectRegister: 'auto'
+    }),
   ],
   resolve: {
     alias: {
