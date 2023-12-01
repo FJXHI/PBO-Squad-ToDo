8c487487b6699a426b5f51235d13b2e5bbca9f62
FJXHI <s82049@informatik.htw-dresden.de>
2023-12-01 19:10:14 +0100

    rm entry add loop

--- src/views/MoreView.vue
+++ src/views/MoreView.vue
@@ -5,16 +5,6 @@ import { useToDoEntryStore } from '@/stores/entry_store'
 
 const store = useToDoEntryStore()
 
-for (let i = 1; i < 21+1; i++) {
-  store.addEntry({
-    title: 'Test Entry ' + i,
-    description: 'Test ' + i + ' Description',
-    color: { r: 255, g: 59, b: 48 },
-    deadline: new Date('2024-01-07'),
-    expenditure: { time: 5, unit: 'h' }
-  })
-}
-
 </script>
 
 <template>
