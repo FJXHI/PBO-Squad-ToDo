7c76824d4ec63ec1ddf75cef9af81c046e45f108
FJXHI <s82049@informatik.htw-dresden.de>
2023-12-01 19:09:15 +0100

    add small entry

---
+++ src/components/SquareEntry.vue
@@ -0,0 +1,114 @@
+<script setup lang="ts">
+import type { ToDoEntry } from '@/stores/entry_store'
+import type { assert } from '@vue/compiler-core'
+import type { PropType, ComponentPublicInstance } from 'vue'
+import { ref, computed, reactive } from 'vue'
+
+const props = defineProps({
+  entry: {
+    type: Object as PropType<ToDoEntry>,
+    required: true
+  }
+})
+
+const entryBox = ref(null)
+let isExpanded = ref(false)
+
+let entry: ToDoEntry = props.entry
+let backgoundColor =
+  'rgba(' +
+  entry.color.r.toString() +
+  ',' +
+  entry.color.g.toString() +
+  ',' +
+  entry.color.b.toString() +
+  ',' +
+  (entry.color.a ? entry.color.a : 255) +
+  ')'
+
+function changeExpand() {
+  isExpanded.value = !isExpanded.value
+}
+
+</script>
+
+<template>
+    <article
+      ref="entryBox"
+      :class="['entry-box', 'stretch-horizontally', isExpanded ? 'detail-height' : 'compact-height']"
+      :style="'--element-color: ' + backgoundColor"
+      @click="changeExpand()"
+    >
+      <div>
+        <h1 class="entry-title">{{ entry?.title ? entry?.title : '' }}</h1>
+        <section class="info-box-1d">
+          <template v-if="entry.deadline != undefined">
+            <span class="entry-text"
+              ><img src="@/assets/icon_deadline.png"/>
+              {{ entry?.deadline.toLocaleDateString() }}</span
+            >
+          </template>
+          <!-- <br v-if="entry.deadline != undefined && entry.expenditure != undefined"> -->
+          <template v-if="entry.expenditure != undefined">
+            <span class="entry-text"
+              ><img src="@/assets/icon_timespan.png" />
+              {{ entry.expenditure.time + ' ' + entry.expenditure.unit }}</span
+            >
+          </template>
+        </section>
+        <template v-if="isExpanded && entry.description != undefined">
+          <p class="entry-text">{{ entry.description }}</p>
+        </template>
+        <span v-if="isExpanded">
+          <nav class="info-btn">
+            <button @click="console.log('delClicked')"><img src="@/assets/icon_delete.svg" /></button>
+            <button @click="console.log('editClicked')"><img src="@/assets/icon_edit.svg" /></button>
+            <button @click="console.log('doneClicked')"><img src="@/assets/icon_done.svg" /></button>
+          </nav>
+        </span>
+      </div>
+    </article>
+</template>
+
+<style scoped>
+.entry-title {
+  font-size: 14pt;
+  font-weight: 600;
+}
+
+.entry-text {
+  font-size: 8pt;
+}
+
+.info-box-1d span {
+  flex: 0 0 50%;
+}
+
+
+.entry-text img {
+    width: 14px;
+    height: auto;
+}
+
+.entry-box {
+    width: 300px;
+  flex: 0 0 33%;
+  background-color: var(--element-color);
+  color: #000000;
+  padding: 1%;
+}
+
+.info-btn button {
+  width: calc(100% / 3);
+  border: none;
+  background-color: transparent;
+}
+
+.info-btn button:hover,
+button:focus,
+button:active {
+  background-color: grey;
+}
+
+
+</style>
--- src/views/MoreView.vue
+++ src/views/MoreView.vue
@@ -1,15 +1,35 @@
+<script setup lang="ts">
+import SquareEntry from '../components/SquareEntry.vue'
+// import Expenditure from '../classes/Expenditure'
+import { useToDoEntryStore } from '@/stores/entry_store'
+
+const store = useToDoEntryStore()
+
+for (let i = 1; i < 21+1; i++) {
+  store.addEntry({
+    title: 'Test Entry ' + i,
+    description: 'Test ' + i + ' Description',
+    color: { r: 255, g: 59, b: 48 },
+    deadline: new Date('2024-01-07'),
+    expenditure: { time: 5, unit: 'h' }
+  })
+}
+
+</script>
+
 <template>
-  <div class="more">
-    <h1>This is more page</h1>
-  </div>
+  <main class="more">
+    <div v-for="entry in store.entries.slice(0, 21)" :key="entry.title">
+      <SquareEntry :entry="entry" :style="'margin-bottom: 5px'"></SquareEntry>
+    </div>
+  </main>
 </template>
 
-<style scoped>
-@media (min-width: 1024px) {
-  .more {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style>
+.more {
+  display: flex;
+  flex-wrap: wrap;
+  /* 3 Elements per row? */
 }
+
 </style>
