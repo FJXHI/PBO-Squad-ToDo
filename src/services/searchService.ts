e22c117d6563be6e33bfe31ec491c50111abee05
HansLuft778 <hendrik.lante@gmail.com>
2023-12-04 19:58:51 +0100

    formatting

--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -1,17 +1,14 @@
-import { useToDoEntryStore } from "@/stores/entry_store";
-
-
+import { useToDoEntryStore } from '@/stores/entry_store'
 
 export function search(query: string): void {
+  const store = useToDoEntryStore()
 
-    const store = useToDoEntryStore()
+  const entries = store.entries
 
-    const entries = store.entries
+  entries.forEach((entry) => {
+    const contains = entry.todoEntry.title.toLowerCase().includes(query.toLowerCase())
+    console.log(contains)
 
-    entries.forEach(entry => {
-        const contains = entry.todoEntry.title.toLowerCase().includes(query.toLowerCase())
-        console.log(contains);
-
-        entry.isVisible = contains ? true : false
-    });
+    entry.isVisible = contains ? true : false
+  })
 }
