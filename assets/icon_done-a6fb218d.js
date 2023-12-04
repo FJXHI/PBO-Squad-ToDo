70813456def48a433e613cf56008cb4b1d515a3e
FJXHI <FJXHI@users.noreply.github.com>
2023-12-04 21:34:56 +0000

    deploy: 9e76714589158f5003f84a342054238157ffebbe

---
+++ assets/icon_done-a6fb218d.js
@@ -0,0 +1,1 @@
+const s="/PBO-Squad-ToDo/assets/icon_delete-ec75e756.svg",o="/PBO-Squad-ToDo/assets/icon_edit-f3f51206.svg",e="/PBO-Squad-ToDo/assets/icon_done-01edd534.svg";export{s as _,o as a,e as b};
