22f1ff8a2835a3ef31244690fcb4a7f01e44547c
HansLuft778 <hendrik.lante@gmail.com>
2023-12-04 20:28:31 +0100

    added multi word search

--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -2,13 +2,16 @@ import { useToDoEntryStore } from '@/stores/entry_store'
 
 export function search(query: string): void {
   const store = useToDoEntryStore()
-
   const entries = store.entries
 
-  entries.forEach((entry) => {
-    const contains = entry.todoEntry.title.toLowerCase().includes(query.toLowerCase())
-    console.log(contains)
+  const searchParams = query.trim().split(' ')
+  console.log(searchParams)
 
+  entries.forEach((entry) => {
+    // const contains = entry.todoEntry.title.toLowerCase().includes(query.toLowerCase())
+    const contains = searchParams.some((param) =>
+      entry.todoEntry.title.toLowerCase().includes(param.toLowerCase())
+    ) // some vs every?
     entry.isVisible = contains ? true : false
   })
 }
