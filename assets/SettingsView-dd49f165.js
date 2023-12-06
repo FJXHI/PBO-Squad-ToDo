c90089ec2ad5a40bdf623ecd3ac691a630a7ade5
FJXHI <FJXHI@users.noreply.github.com>
2023-12-06 16:22:03 +0000

    deploy: c4a9089145b423c482e179ad9ad5fff7ef4935e8

---
+++ assets/SettingsView-dd49f165.js
@@ -0,0 +1,1 @@
+import{_ as s,o as t,c as _,p as o,g as c,a}from"./index-254b8378.js";const n={},d=e=>(o("data-v-a68d32b3"),e=e(),c(),e),i={class:"settings"},p=d(()=>a("h1",null,"This is the settings page",-1)),r=[p];function h(e,l){return t(),_("div",i,r)}const u=s(n,[["render",h],["__scopeId","data-v-a68d32b3"]]);export{u as default};
