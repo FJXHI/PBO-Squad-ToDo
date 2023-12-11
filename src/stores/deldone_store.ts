53bca8fc162bc977274c67dc12adc5fd5055195e
FJXHI <s82049@informatik.htw-dresden.de>
2023-12-11 09:28:03 +0100

    add del done Store

---
+++ src/stores/deldone_store.ts
@@ -0,0 +1,49 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export interface Color {
+  r: number
+  g: number
+  b: number
+  a?: number
+}
+export interface Tag {
+  tag: string
+  tagColor: Color
+}
+
+export interface ToDoEntryInfo {
+  title: string
+  description?: string
+  color: Color
+
+  deadline?: Date
+  expenditure?: { time: number; unit: string }
+
+  flatPriority?: number
+
+  tags?: Tag[]
+}
+
+export interface ToDoEntry {
+  todoEntry: ToDoEntryInfo
+  isVisible: boolean
+  isdeleted: boolean
+}
+
+export const useToDoEntryStore = defineStore('todoEntries', () => {
+  const entries = ref<ToDoEntry[]>([])
+
+  function addEntry(entry: ToDoEntry): void {
+    entries.value.push(entry)
+  }
+
+  function removeEntry(entry: ToDoEntry): void {
+    const index = entries.value.indexOf(entry)
+    if (index > -1) {
+      entries.value.splice(index, 1)
+    }
+  }
+
+  return { entries, addEntry, removeEntry }
+})
