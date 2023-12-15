4071d7efa99320b1a53831ed454bf2c9164214d4
4rgos <s83627@informatik.htw-dresden.de>
2023-12-15 19:49:06 +0100

    Add swipe without moving the element outside of the screen

--- src/components/CompactEntry.vue
+++ src/components/CompactEntry.vue
@@ -3,6 +3,8 @@ import type { ToDoEntry } from '@/stores/entry_store'
 import type { assert } from '@vue/compiler-core'
 import type { PropType, ComponentPublicInstance } from 'vue'
 import { ref, computed, reactive } from 'vue'
+import { useElementSize, useSwipe } from '@vueuse/core'
+import type { UseSwipeDirection } from '@vueuse/core'
 
 const props = defineProps({
   entry: {
@@ -13,6 +15,9 @@ const props = defineProps({
 
 const entryBox = ref(null)
 let isExpanded = ref(false)
+
+const content = ref(null)
+const initialContentSize = {width: useElementSize(content).width, height: useElementSize(content).height}
 
 let entry: ToDoEntry = props.entry
 let backgoundColor =
@@ -29,50 +34,106 @@ let backgoundColor =
 function changeExpand() {
   isExpanded.value = !isExpanded.value
 }
+
+
+let deleteWidth = ref(0);
+let tickWidth = ref(0);
+const {direction, isSwiping, lengthX, lengthY} = useSwipe(
+  entryBox,
+  {
+    passive: true,
+    onSwipe(e: TouchEvent){
+      if (lengthX.value < 0)
+        deleteWidth.value = Math.abs(lengthX.value)
+      else
+        tickWidth.value = Math.abs(lengthX.value)
+    },
+    onSwipeEnd(e: TouchEvent, direction: UseSwipeDirection){
+      // check for enough swipe for tick/delete
+      // do stuff if critera is met
+
+      // reset swipe chanes if not
+      deleteWidth.value = tickWidth.value = 0;
+    }
+  }
+)
+
 </script>
 
 <template>
-  <article
+  <span class="horizontal-box stretch-horizontally">
+    <!-- delete tooltip-->
+    <aside class="delete-box restrict-size"
+      :style="`width: ${deleteWidth}px;`">
+      <img alt="" class="icon no-padding" src="@/assets/icon_delete.svg" />
+    </aside>
+
+    <!-- main entry content -->
+    <article
     ref="entryBox"
-    :class="['entry-box', 'stretch-horizontally', isExpanded ? 'detail-height' : 'compact-height']"
-    :style="'--element-color: ' + backgoundColor"
-    @click="changeExpand()"
-  >
-    <!-- <div class="center-vertically"> -->
-    <div>
-      <h1 class="entry-title">{{ entry?.title ? entry?.title : '' }}</h1>
-      <section class="info-box-1d">
-        <template v-if="entry.deadline != undefined">
-          <span class="entry-text"
-            ><img src="@/assets/icon_deadline.png" />
-            {{ entry?.deadline.toLocaleDateString() }}</span
-          >
+    :class="['entry-box', isExpanded ? 'detail-height' : 'compact-height']"
+    :style="`--element-color: ${backgoundColor};`"
+    @click="changeExpand()">
+      <div ref="content" 
+        :style="`width: ${initialContentSize.width}px;
+        `">
+        <!-- transform: translateX(${deleteWidth-tickWidth}px); -->
+        <h1 class="entry-title">{{ entry?.title ? entry?.title : '' }}</h1>
+        <section class="info-box-1d">
+          <template v-if="entry.deadline != undefined">
+            <span class="entry-text"
+              ><img src="@/assets/icon_deadline.png" />
+              {{ entry?.deadline.toLocaleDateString() }}</span
+            >
+          </template>
+          <template v-if="entry.expenditure != undefined">
+            <span class="entry-text"
+              ><img src="@/assets/icon_timespan.png" />
+              {{ entry.expenditure.time + ' ' + entry.expenditure.unit }}</span
+            >
+          </template>
+        </section>
+        <template v-if="isExpanded && entry.description != undefined">
+          <p class="entry-text">{{ entry.description }}</p>
         </template>
-        <template v-if="entry.expenditure != undefined">
-          <span class="entry-text"
-            ><img src="@/assets/icon_timespan.png" />
-            {{ entry.expenditure.time + ' ' + entry.expenditure.unit }}</span
-          >
-        </template>
-      </section>
-      <template v-if="isExpanded && entry.description != undefined">
-        <p class="entry-text">{{ entry.description }}</p>
-      </template>
-      <span v-if="isExpanded">
-        <nav class="info-box-1d">
-          <button @click="console.log('delClicked')"><img src="@/assets/icon_delete.svg" /></button>
-          <button @click="console.log('editClicked')"><img src="@/assets/icon_edit.svg" /></button>
-          <button @click="console.log('doneClicked')"><img src="@/assets/icon_done.svg" /></button>
-        </nav>
-      </span>
-    </div>
-  </article>
+        <span v-if="isExpanded">
+          <nav class="info-box-1d">
+            <button @click="console.log('delClicked')"><img src="@/assets/icon_delete.svg" /></button>
+            <button @click="console.log('editClicked')"><img src="@/assets/icon_edit.svg" /></button>
+            <button @click="console.log('doneClicked')"><img src="@/assets/icon_done.svg" /></button>
+          </nav>
+        </span>
+      </div>
+
+    </article>
+
+    <!-- accept tooltip -->
+    <aside class="tick-box restrict-size"
+      :style="`width: ${tickWidth}px;`">
+      <img alt="" class="icon no-padding" src="@/assets/icon_done.svg" />
+    </aside>
+
+  </span>
+  
 </template>
 
 <style scoped>
 .entry-title {
   font-size: 27pt;
   font-weight: 600;
+}
+
+.tick-box {
+  background-color: green;
+}
+
+.delete-box {
+  background-color: red;
+}
+
+.restrict-size {
+  flex-grow: 0;
+  max-width: 40vw;
 }
 
 .info-box-1d {
@@ -92,6 +153,11 @@ function changeExpand() {
   background-color: transparent;
 }
 
+.horizontal-box {
+  display: flex;
+  position: relative;
+}
+
 .info-box-1d button:hover,
 button:focus,
 button:active {
@@ -106,8 +172,10 @@ button:active {
   /* border-radius: 5px; */
 
   background-color: var(--element-color);
+  /* min-width: 100vh; */
 
   padding: 10px;
+  flex-grow: 1;
 
   color: #000000;
   position: relative;
@@ -126,6 +194,10 @@ button:active {
 
 .detail-height {
   min-height: 16vh;
+}
+
+.no-padding {
+  padding: 0;
 }
 
 .center-vertically {
