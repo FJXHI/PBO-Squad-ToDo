f3dab96f1adc2113394f15e6113a47cd16d0b79b
FJXHI <FJXHI@users.noreply.github.com>
2023-12-16 11:04:07 +0000

    deploy: f1f14d551581e1409f0715fbd5df47ff82ec2199

---
+++ assets/AboutView-3f5d7886.js
@@ -0,0 +1,1 @@
+import{_ as o,o as s,c as t,p as _,g as c,a}from"./index-a39f4721.js";const d={},n=e=>(_("data-v-6ee4f2ed"),e=e(),c(),e),p={class:"about"},i=n(()=>a("h1",null,"This is an about page",-1)),r=[i];function u(e,f){return s(),t("div",p,r)}const h=o(d,[["render",u],["__scopeId","data-v-6ee4f2ed"]]);export{h as default};
