d7a72db03f36a1e57721c6368fca2f96024e1bd4
HansLuft778 <hendrik.lante@gmail.com>
2023-12-16 01:06:03 +0100

    added search store

--- src/stores/search_store.ts
+++ src/stores/search_store.ts
@@ -1,19 +1,21 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useSearchStore = defineStore('searchSettings', () => {
-  // const sortOptions = ref([
-  //   { name: 'Title', isActive: false, descending: true },
-  //   { name: 'Deadline', isActive: false, descending: true },
-  //   { name: 'Expenditure', isActive: false, descending: true },
-  //   { name: 'Last Added', isActive: false, descending: true }
-  // ])
+interface SortOptions {
+  [key: string]: {
+    isActive: boolean
+    isDescending: boolean
+  }
+}
 
-  const sortOptions = ref({
-    Title: { isActive: false, descending: true },
-    Deadline: { isActive: false, descending: true },
-    Expenditure: { isActive: false, descending: true },
-    LastAdded: { isActive: false, descending: true }
+export const useSearchStore = defineStore('searchSettings', () => {
+
+
+  const sortOptions = ref<SortOptions>({
+    Title: { isActive: true, isDescending: true },
+    Deadline: { isActive: false, isDescending: true },
+    Expenditure: { isActive: false, isDescending: true },
+    LastAdded: { isActive: false, isDescending: true }
   })
 
   return { sortOptions }
