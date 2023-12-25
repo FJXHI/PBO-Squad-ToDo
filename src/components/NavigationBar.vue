e2a1e719cd3aa48ef2f790c23bb05bdeb4ce649c
HansLuft778 <hendrik.lante@gmail.com>
2023-12-25 17:04:15 +0100

    updated navbar to use tailwind

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,55 +1,34 @@
-<script lang="ts">
+<script setup lang="ts">
 import NavigationButton from '@/components/NavigationButton.vue'
-export default {
-  components: {
-    NavigationButton
-  },
-  methods: {
-    homeClicked() {
-      console.log('homeClicked')
-      this.$router.push('/')
-    },
-    addClicked() {
-      console.log('addClicked')
-      this.$router.push('/add')
-    },
-    moreClicked() {
-      console.log('moreClicked')
-      this.$router.push('/more')
-    }
-  }
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function homeClicked() {
+  router.push('/')
+}
+function addClicked() {
+  router.push('/add')
+}
+function moreClicked() {
+  router.push('/more')
 }
 </script>
 
 <template>
   <div class="nav">
     <div class="nav-field">
-      <span class="nav_bar">
-        <NavigationButton class="btn_home" @click="homeClicked">
-          <img alt="Home" class="icon" src="@/assets/icon_density_small.svg" />
+      <span class="flex">
+        <NavigationButton class="flex justify-center items-center" @click="homeClicked">
+          <img alt="Home" class="h-[50%]" src="@/assets/icon_density_small.svg" />
         </NavigationButton>
-        <NavigationButton class="btn_add" @click="addClicked">
-          <img alt="Add" class="icon" src="@/assets/icon_add.svg" />
+        <NavigationButton class="flex justify-center items-center" @click="addClicked">
+          <img alt="Add" class="h-[50%]" src="@/assets/icon_add.svg" />
         </NavigationButton>
-        <NavigationButton class="btn_overview" @click="moreClicked">
-          <img alt="More" class="icon" src="@/assets/icon_grid_view.svg" />
+        <NavigationButton class="flex justify-center items-center" @click="moreClicked">
+          <img alt="More" class="h-[50%]" src="@/assets/icon_grid_view.svg" />
         </NavigationButton>
       </span>
     </div>
   </div>
 </template>
-
-<style>
-.nav_bar {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  background-color: #1c1c1e;
-}
-
-.icon {
-  padding: 1vh;
-  height: 70%;
-  width: 70%;
-}
-</style>
