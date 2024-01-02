50a1a8b3d3488fd0bffdfe862f7e3c8cccd64cef
HansLuft778 <hendrik.lante@gmail.com>
2024-01-02 17:36:20 +0100

    simplified nav button css

--- src/components/NavigationButton.vue
+++ src/components/NavigationButton.vue
@@ -12,17 +12,8 @@
 
 <style>
 .navbtn {
-  outline: none;
-  border: none;
   width: calc(100% / 3);
   height: 8vh;
-  color: #fff;
   background-color: #1c1c1e;
-  font-size: 8px;
-  letter-spacing: 1px;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: 0.5s;
-  transition-property: border-left, border-right, box-shadow;
 }
 </style>
