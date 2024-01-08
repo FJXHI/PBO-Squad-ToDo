d1a4a3c060692187072293df440267cee088199b
HansLuft778 <hendrik.lante@gmail.com>
2024-01-08 17:37:51 +0100

    Remove unused import statement in CompactEntry.vue

--- src/components/CompactEntry.vue
+++ src/components/CompactEntry.vue
@@ -2,7 +2,6 @@
 import type { ToDoEntry, ToDoEntryInfo } from '@/stores/entry_store'
 import { useToDoEntryStore } from '@/stores/entry_store'
 import { useDeleteStore } from '@/stores/delete_done_store'
-import { removeAndAddEntry } from '@/services/DeleteDoneService'
 import type { assert } from '@vue/compiler-core'
 import type { PropType, ComponentPublicInstance } from 'vue'
 import { useElementSize, useSwipe } from '@vueuse/core'
