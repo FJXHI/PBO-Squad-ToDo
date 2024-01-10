6299b0f9905947499a7f3ae8b437340664c61586
Felix <helmert.felix+git@gmail.com>
2024-01-10 11:43:12 +0100

    add Tag List Dropdown

---
+++ src/components/TagDropdown.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const inputTags = ref('')
+const tagData = ['Uni', 'Work', 'Test']
+</script>
+
+<template>
+  <input
+    list="tag-list"
+    id="id_tags"
+    class="user-input duration"
+    placeholder="Tags"
+    v-model="inputTags"
+  />
+
+  <datalist id="tag-list">
+    <option v-for="tag in tagData" :key="tag" :value="tag"></option>
+  </datalist>
+</template>
+
+<style scoped></style>
