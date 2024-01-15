7793b1e6b73d06ef1cef6b8add58fa34df152c8f
HansLuft778 <HansLuft778@users.noreply.github.com>
2024-01-15 15:58:47 +0000

    deploy: 779d1c136c390079954ed27ea4efc09bb46c25e2

---
+++ assets/AboutView-5818714f.js
@@ -0,0 +1,1 @@
+import{_ as o,f as s,j as t,z as _,A as c,k as a}from"./index-8369a679.js";const d={},n=e=>(_("data-v-6ee4f2ed"),e=e(),c(),e),p={class:"about"},i=n(()=>a("h1",null,"This is an about page",-1)),r=[i];function u(e,f){return s(),t("div",p,r)}const h=o(d,[["render",u],["__scopeId","data-v-6ee4f2ed"]]);export{h as default};
