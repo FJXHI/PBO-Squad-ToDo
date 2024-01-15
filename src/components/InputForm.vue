e6036125ecc82bd71230b9782e4adb1cdf6816da
FJXHI <s82049@informatik.htw-dresden.de>
2024-01-15 09:08:41 +0100

    duration only accept numbers

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -63,8 +63,9 @@ const saveEdit = () => {
       deadlineDate = undefined
     }
     let timeExpenditure
-    if (inputDuration.value) {
-      timeExpenditure = { time: parseInt(inputDuration.value), unit: inputDurationUnit.value }
+    const durationValue = inputDuration.value.trim().replace(/\D/g, '') // remove all non-numeric characters
+    if (durationValue !== '') {
+      timeExpenditure = { time: parseInt(durationValue), unit: inputDurationUnit.value }
     } else {
       timeExpenditure = undefined
     }
