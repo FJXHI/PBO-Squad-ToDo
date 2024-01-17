0e1b01b46e1f082fbfd8eb37994d42aeccb742bc
HansLuft778 <HansLuft778@users.noreply.github.com>
2024-01-17 20:36:16 +0000

    deploy: 12febb71a9965c8a502b8b33b1d06539a90076df

---
+++ app/assets/AddView-de570ff6.js
@@ -0,0 +1,1 @@
+import{e as n,A as t,h as r,B as a}from"./index-a87f5d3e.js";import{_ as c}from"./InputForm.vue_vue_type_style_index_0_lang-df415742.js";const m=n({__name:"AddView",setup(i){const o=t();function s(){console.log("CloseAddView"),o.push("/")}return(p,e)=>(r(),a(c,{onCloseaction:e[0]||(e[0]=u=>s())}))}});export{m as default};
