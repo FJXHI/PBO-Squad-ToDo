4d93024c28bca013df2ada52c2dc640bd91f1136
FJXHI <FJXHI@users.noreply.github.com>
2024-01-19 13:27:52 +0000

    deploy: 35101a42054be4dae9037be912e67cc8748c99e5

---
+++ docs/assets/js/5e95c892.e4d21a4a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpbo_docs=self.webpackChunkpbo_docs||[]).push([[661],{1892:(e,s,r)=>{r.r(s),r.d(s,{default:()=>p});r(7294);var c=r(6010),u=r(1944),a=r(5281),o=r(8790),d=r(7961),n=r(5893);function p(e){return(0,n.jsx)(u.FG,{className:(0,c.Z)(a.k.wrapper.docsPages),children:(0,n.jsx)(d.Z,{children:(0,o.H)(e.route.routes)})})}}}]);
