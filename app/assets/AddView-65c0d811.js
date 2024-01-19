5bea7dd1ae72d0c0dc4a6d6c6985b4d5401a6828
FJXHI <FJXHI@users.noreply.github.com>
2024-01-19 17:32:34 +0000

    deploy: 38c1cdef21df3930d8348fb773bed2f6be16c9e5

---
+++ app/assets/AddView-65c0d811.js
@@ -0,0 +1,1 @@
+import{e as n,z as s,h as r,A as a}from"./index-bd7e6fd4.js";import{_ as c}from"./InputForm.vue_vue_type_style_index_0_lang-e5209d6b.js";const f=n({__name:"AddView",setup(i){const o=s();function t(){o.push("/")}return(p,e)=>(r(),a(c,{onCloseaction:e[0]||(e[0]=u=>t())}))}});export{f as default};
